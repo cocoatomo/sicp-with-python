d9fd8e5f1dac0f8fc575a7cdf6d4c09d951c2852
cocoatomo <cocoatomo77@gmail.com>
2011-11-22 03:34:52 +0900

    exercise 1.7

--- chap1.py
+++ chap1.py
@@ -1,7 +1,9 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-print('Exercise 1.5')
+print('''
+Exercise 1.5
+============''')
 
 def p():
     p()
@@ -14,7 +16,9 @@ def test(x, y):
 
 print(test(0, p))
 
-print("1.1.7 Example: Square Roots by Newton's Method")
+print('''
+1.1.7 Example: Square Roots by Newton's Method
+==============================================''')
 
 def average(x, y):
     return (x + y) / 2
@@ -44,5 +48,56 @@ def _test():
     print(sqrt(sqrt(2) + sqrt(3)))
     print(square(sqrt(1000)))
 
-if __name__ == '__main__':
-    _test()
+_test()
+
+
+print('''
+Exercise 1.7
+============''')
+
+print('''
+good_enough(10**10, 10**10 + 1)
+expect: True, actual: {0}'''.format(good_enough(10**10, 10**10 + 1)))
+
+print('''
+good_enough(0.00001, 0.0001)
+expect: False, actual: {0}'''.format(good_enough(0.0001, 0.0009)))
+
+print('re-define "good_enough" to show calculation process')
+
+def good_enough(guess, x):
+    print('guess: {0}'.format(guess))
+    return abs(square(guess) - x) < 0.001
+
+print('''
+sqrt(10**10) (obviously is 10**5)
+---------------------------------''')
+print('actual: {0}'.format(sqrt(10**10)))
+
+print('''
+sqrt(0.0001) (obviously is 0.01)
+----------------------------------''')
+print('actual: {0}'.format(sqrt(0.0001)))
+
+print('''
+refining good_enough function...
+
+def good_enough(guess, x):
+    print('guess: {0}'.format(guess))
+    return abs(square(guess) / x - 1) < 0.001''')
+
+print('re-run')
+
+def good_enough(guess, x):
+    print('guess2: {0}'.format(guess))
+    return abs((square(guess) / x) - 1) < 0.001
+
+print('''
+sqrt(10**10) (obviously is 10**5)
+---------------------------------''')
+print('actual: {0}'.format(sqrt(10**10)))
+
+print('''
+sqrt(0.0001) (obviously is 0.01)
+----------------------------------''')
+print('actual: {0}'.format(sqrt(0.0001)))
