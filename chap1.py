85adcdb194dc25e9cc0fc40319c6b69b9cbc695a
cocoatomo <cocoatomo77@gmail.com>
2013-05-20 02:10:17 +0900

    fast-prime in 1.2.6

--- chap1.py
+++ chap1.py
@@ -263,13 +263,17 @@ h(n) = 2**2**...**2 (<- n 2's) if n > 0 else 0
 
 
 def test_f():
-    for n in xrange(10):
+    for n in range(10):
         assert f(n) == 2 * n
+
+test_f()
 
 
 def test_g():
-    for n in xrange(10):
+    for n in range(10):
         assert g(n) == (2**n if n > 0 else 0)
+
+test_g()
 
 
 def test_h():
@@ -278,13 +282,14 @@ def test_h():
             return 0
 
         result = 1
-        for i in xrange(n):
+        for i in range(n):
             result = 2**result
         return result
 
-    for n in xrange(5):
+    for n in range(5):
         assert h(n) == hh(n)
 
+test_h()
 
 print('''
 1.2.2  Tree Recursion
@@ -313,6 +318,8 @@ def test_fib():
     assert fib(7) == 13
     assert fib(8) == 21
     assert fib(9) == 34
+
+test_fib()
 
 
 print('''
@@ -350,6 +357,7 @@ def count_change(amount):
 def test_cc():
     assert count_change(100) == 292
 
+test_cc()
 
 print('''
 Exercise 1.11
@@ -375,6 +383,8 @@ def test_f():
     assert f(7) == 142
     assert f(8) == 335
     assert f(9) == 796
+
+test_f()
 
 
 def f(n):
@@ -422,6 +432,21 @@ Exercise 1.13
 
 print('proof: obvious.')
 
+print('''
+1.2.3  Orders of Growth
+=======================''')
+
+print('''
+Exercise 1.14
+=============''')
+
+pass
+
+print('''
+Exercise 1.15
+=============''')
+
+pass
 
 print('''
 1.2.4  Exponentiation
@@ -444,6 +469,8 @@ def test_expt():
         for n in range(10):
             assert expt(b, n) == b**n
 
+test_expt()
+
 
 def even(n):
     return (n % 2) == 0
@@ -463,8 +490,16 @@ def test_fast_expt():
         for n in range(10):
             assert fast_expt(b, n) == b**n
 
+test_fast_expt()
+
+print('''
+Exercise 1.16
+=============''')
+
 
 def expt_iter(b, exponent, product):
+    """invariant: b**exponent * product
+    """
     if exponent == 0:
         return product
     elif even(exponent):
@@ -475,3 +510,218 @@ def expt_iter(b, exponent, product):
 
 def test_expt2():
     test_expt()
+
+test_expt2()
+
+print('''
+Exercise 1.17
+=============''')
+
+
+def halve(b):
+    return b // 2
+
+
+def double(n):
+    return n + n
+
+
+def fast_int_mul(a, b):
+    if b < 0:
+        return -fast_int_mul(a, -b)
+    elif b == 0:
+        return 0
+    elif even(b):
+        return double(fast_int_mul(a, halve(b)))
+    else:
+        return a + fast_int_mul(a, b - 1)
+
+
+def test_fast_int_mul():
+    for a in range(-5, 10):
+        for b in range(-5, 10):
+            assert fast_int_mul(a, b) == a * b
+
+test_fast_int_mul()
+
+print('''
+Exercise 1.18
+=============''')
+
+
+def int_mul_iter(a, multiplier, summation):
+    if multiplier == 0:
+        return summation
+    elif even(multiplier):
+        return int_mul_iter(double(a), halve(multiplier), summation)
+    else:
+        return int_mul_iter(double(a), halve(multiplier - 1), a + summation)
+
+
+def int_mul(a, b):
+    return int_mul_iter(a, b, 0)
+
+
+def test_int_mul():
+    for a in range(10):
+        for b in range(10):
+            assert int_mul(a, b) == a * b
+
+test_int_mul()
+
+print('''
+Exercise 1.19
+=============''')
+
+
+def fib(n):
+    return fib_iter(1, 0, 0, 1, n)
+
+
+def fib_iter(a, b, p, q, count):
+    """fibonacci series.
+
+    fib(0) = 0, fib(1) = 1, fib(2) = 1,...
+
+    fib(n) = b_n
+
+    a_0 = 1, b_0 = 0
+
+    (a_n) = T_{p q}^n (a_0)
+    (b_n)             (b_0)
+
+    T_{p q} = (p + q q)
+              (q     p)
+
+    T_{0 1} = (1 1)
+              (1 0)
+    """
+    if count == 0:
+        return b
+    elif even(count):
+        return fib_iter(a,
+                        b,
+                        square(p) + square(q),
+                        2 * p * q + square(q),
+                        count // 2)
+    else:
+        return fib_iter(b * q + a * q + a * p,
+                        b * p + a * q,
+                        p,
+                        q,
+                        count - 1)
+
+
+def test_fib2():
+    test_fib()
+
+test_fib2()
+
+print('''
+1.2.5  Greatest Common Divisors
+===============================''')
+
+print('''
+Exercise 1.20
+=============''')
+
+pass
+
+print('''
+1.2.6  Example: Testing for Primality
+=====================================''')
+
+print('''
+Exercise 1.21
+=============''')
+
+
+def divides(a, b):
+    return (b % a) == 0
+
+
+def find_divisor(n, test_divisor):
+    if square(test_divisor) > n:
+        return n
+    elif divides(test_divisor, n):
+        return test_divisor
+    else:
+        return find_divisor(n, test_divisor + 1)
+
+
+def smallest_divisor(n):
+    return find_divisor(n, 2)
+
+
+def prime(n):
+    return n == smallest_divisor(n)
+
+
+def test_prime():
+    assert prime(2)
+    assert prime(3)
+    assert not prime(4)
+    assert prime(5)
+    assert not prime(6)
+    assert prime(7)
+    assert not prime(8)
+    assert not prime(9)
+    assert not prime(10)
+    assert prime(11)
+
+test_prime()
+
+
+def expmod(base, exp, m):
+    if exp == 0:
+        return 1
+    elif even(exp):
+        return square(expmod(base, exp // 2, m)) % m
+    else:
+        return (base * expmod(base, exp - 1, m)) % m
+
+
+def test_expmod():
+    assert expmod(1, 1, 2) == 1
+    assert expmod(1, 2, 2) == 1
+    assert expmod(2, 2, 3) == 1
+    assert expmod(2, 3, 4) == 0
+    assert expmod(4, 2, 5) == 1
+    assert expmod(2, 3, 6) == 2
+    assert expmod(3, 2, 7) == 2
+
+
+test_expmod()
+
+
+def fermat_test(n):
+    def try_it(a):
+        return expmod(a, n, n) == a
+
+    import random
+
+    return try_it(1 + random.randrange(n - 1))
+
+
+def fast_prime(n, times):
+    if times == 0:
+        return True
+    elif fermat_test(n):
+        return fast_prime(n, times - 1)
+    else:
+        return False
+
+
+def test_fast_prime(times):
+    assert fast_prime(2, times)
+    assert fast_prime(3, times)
+    assert not fast_prime(4, times)
+    assert fast_prime(5, times)
+    assert not fast_prime(6, times)
+    assert fast_prime(7, times)
+    assert not fast_prime(8, times)
+    assert not fast_prime(9, times)
+    assert not fast_prime(10, times)
+    assert fast_prime(11, times)
+
+test_fast_prime(10)
